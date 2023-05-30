d24f42acf7029fc0748253020bc7222dffc987f1
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-30 16:48:09 +0200

    Implementazione milestone 3 e 4

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,29 @@
 <script>
+import axios from 'axios';
 export default {
     name: 'AppMain',
     data() {
         return {
+            progetti: [],
+            baseUrl: 'http://localhost:8000',
 
         }
+    },
+    methods: {
+        getProgetto() {
+            axios.get(`${this.baseUrl}/api/progetti`)
+                .then(response => {
+                    console.log(response);
+                    this.progetti = response.data.results;
+
+                });
+
+
+        }
+
+    },
+    mounted() {
+        this.getProgetto();
     }
 
 }
