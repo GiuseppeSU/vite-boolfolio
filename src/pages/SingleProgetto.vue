41b39ed2fb15c1cdb712df9e8d0720956a07d63c
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-31 16:22:18 +0200

    Fine esercizio

--- src/pages/SingleProgetto.vue
+++ src/pages/SingleProgetto.vue
@@ -1,9 +1,30 @@
 <script>
+import axios from 'axios';
+import { store } from '../store.js';
+
 export default {
-    name: 'SingleProgetto'
+    name: 'SingleProgetto',
+    data() {
+        return {
+            store,
+
+        }
+    },
+    mounted() {
+        const slug = this.$route.params.slug;
+
+
+
+        axios.get(`${this.store.baseUrl}/api/progetto/${slug}`)
+            .then(response => {
+                console.log(response);
+            });
+
+    }
 }
+
 </script>
 
 <template>
     <h1>qui ci sono i post singoli</h1>
-</template>
+</template >
